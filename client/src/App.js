2b3535d95d16624628c1093342b9040ca4d9b6aa
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-07-02 14:26:28 -0700

    Delete React boilerplate artifacts

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
-import './App.css';
 
 // apollo client setup
 const client = new ApolloClient({
@@ -14,13 +12,7 @@ class App extends Component {
     return (
       <ApolloProvider client={client}>
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h1>Brandi's Recipe Directory</h1>
         </div>
       </ApolloProvider>
     );
