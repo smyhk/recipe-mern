dbe07ff1bb83eb046a86cd6d6de59a29ebc364c7
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-07-11 21:09:52 -0700

    Move year function to state

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
 class Footer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { year: new Date().getFullYear() };
+  }
+
   render() {
     return (
       <footer className="footer">
         <div className="container">
-          <span className="text-muted">{"Brandi's Recipe Site ©"} {new Date().getFullYear()}</span>
+          <span className="text-muted">Brandi's Recipe Site &copy; {this.state.year}</span>
         </div>
       </footer>
     );
