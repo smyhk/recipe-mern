0cd40d89dea88e02ddded34f4ec2647b1bb27384
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-07-17 15:43:35 -0700

    Refactor recipe list

--- client/src/components/RecipeList.js
+++ client/src/components/RecipeList.js
@@ -1,25 +1,33 @@
-import React, { Component } from 'react';
-import { graphql } from 'react-apollo'; // for binding to component
-import { getRecipesQuery } from '../queries/queries';
+import React, { Component } from "react";
+import { graphql } from "react-apollo"; // for binding to component
+import { getRecipesQuery } from "../queries/queries";
 
 // components
-import RecipeDetails from './RecipeDetails';
+import RecipeDetails from "./RecipeDetails";
 
 class RecipeList extends Component {
   constructor(props) {
     super(props);
     this.state = {
       selected: null
-    }
+    };
   }
   displayRecipes() {
     var data = this.props.data;
     if (data.loading) {
-      return( <div>Loading recipes...</div>);
+      return <div>Loading recipes...</div>;
     } else {
-      return data.recipes.slice(-5,).map(recipe => {
-        return(
-          <li className="list-group-item" key={ recipe.id } onClick={ () => { this.setState({ selected: recipe.id }) } }>{ recipe.name }</li>
+      return data.recipes.slice(-5).map(recipe => {
+        return (
+          <li
+            className="list-group-item"
+            key={recipe.id}
+            onClick={() => {
+              this.setState({ selected: recipe.id });
+            }}
+          >
+            {recipe.name}
+          </li>
         );
       });
     }
@@ -27,10 +35,8 @@ class RecipeList extends Component {
   render() {
     return (
       <div className="container">
-        <ul className="list-group">
-          { this.displayRecipes() }
-        </ul>
-        <RecipeDetails recipeId={ this.state.selected }/>
+        <ul className="list-group">{this.displayRecipes()}</ul>
+        <RecipeDetails recipeId={this.state.selected} />
       </div>
     );
   }
